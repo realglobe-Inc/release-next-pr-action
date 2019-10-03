3b45dffca8be3e820b1d57cc9c89d170adbb8d02
ikngtty <ikngtty@gmail.com>
2019-10-03 17:25:46 +0900

    :green_heart: add '_' prefixes for private members

--- src/pr-analyzer.ts
+++ src/pr-analyzer.ts
@@ -1,7 +1,7 @@
 export default class {
-  pr: any
+  _pr: any
   constructor(pr) {
-    this.pr = pr
+    this._pr = pr
   }
 
   baseIssues(): number[] {
@@ -9,7 +9,7 @@ export default class {
 
     const statementReg = /^[ \t]*after[ \t]*((?:#(?:\d+)[ \t]*,?[ \t]*)+)/gim
     let statementMatch
-    while ((statementMatch = statementReg.exec(this.pr.body)) != null) {
+    while ((statementMatch = statementReg.exec(this._pr.body)) != null) {
       const issuesText = statementMatch[1]
 
       const issueReg = /#(\d+)/g
